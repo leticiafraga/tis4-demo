499381c3a09c033feaa1261982a3b38af60f6a5f
Leticia <leticia.fraga@epitech.eu>
2024-12-01 21:15:23 -0300

    test: login test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -6,5 +6,10 @@ describe("Acceptance test", () => {
 
         cy.contains("Entrar", { timeout: 20000 }).click();
         cy.url().should("not.include", "login", { timeout: 20000 });
+
+        cy.url().should("include", "list", { timeout: 20000 });
+        cy.contains("Lista de Atletas", { timeout: 20000 }).should(
+            "be.visible"
+        );
     });
 });
